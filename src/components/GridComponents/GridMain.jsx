bc93025e4c9d77deb9bd18f03d178cb62f5f597e
your_username <sardaralimuradali4@gmail.com>
2023-03-05 20:35:33 +0500

    first commit

---
+++ src/components/GridComponents/GridMain.jsx
@@ -0,0 +1,130 @@
+// This is the main part of the grid where we have the List of the Users with their task and date stuff and is using the data.js to take the dummy data
+
+import React from "react";
+import { AiFillCaretDown } from "react-icons/ai";
+import { AiFillCaretUp } from "react-icons/ai";
+import axios from "axios";
+import { useSelector, useDispatch } from "react-redux";
+import { handleTotalPages, setData ,handleDescriptionSort,
+  handleLocationSort,
+  handleTitleSort} from "../../features/pageSlice";
+import CircularProgress from '@mui/material/CircularProgress';
+
+const GridMain = () => {
+  let [loading,setLoading]=React.useState(true)
+  let dispatch = useDispatch();
+  // let {data:dummyData}=useSelector((state)=>state.store)
+  // console.log(data)
+  let {
+    paginationPage,
+    selectPage,
+    search,
+    data: dummyData,
+  } = useSelector((state) => state.store);
+
+
+  
+  // React.useEffect(()=>{
+  //    dispatch(setData({ data: data }));
+  // },[])
+
+
+  React.useEffect(() => {
+    dispatch(handleTotalPages({ pages: Math.ceil(dummyData.length / selectPage) }));
+  }, [selectPage]);
+
+  React.useEffect(()=>{
+    let start=async ()=>{
+      try {
+        setLoading(true)
+        let {data}=await axios.post("https://searchjobserver.herokuapp.com/JobSearch/crawler/all",{login:"root",password:"root"})
+        dispatch(setData({ data: data }));
+        setLoading(false)
+      } catch (error) {   
+        console.log(error)
+      }
+    }
+    start()
+  },[])
+
+  const openInNewTab = (url) => {
+    window.open(url, '_blank', 'noreferrer');
+  };
+
+  if(loading){
+    return  <CircularProgress />
+  }
+
+
+
+
+  return (
+    <div className="TableDataMain">
+      <div>
+        <div className="flex-even gridHead">
+          <div className="gridHeadSingle" onClick={()=>dispatch(handleTitleSort())}>
+            <a>Title</a>
+            <div className="gridHeadIcons">
+              <AiFillCaretUp className="IconUp" />
+              <AiFillCaretDown className="IconDown" />
+            </div>
+          </div>
+          <div className="gridHeadSingle" onClick={()=>dispatch(handleLocationSort())}>
+            <a>Location</a>
+            <div className="gridHeadIcons">
+              <AiFillCaretUp className="IconUp" />
+              <AiFillCaretDown className="IconDown" />
+            </div>
+          </div>
+
+          <div className="gridHeadSingle" onClick={()=>dispatch(handleDescriptionSort())}>
+            <a >Description</a>
+            <div className="gridHeadIcons">
+              <AiFillCaretUp className="IconUp" />
+              <AiFillCaretDown className="IconDown" />
+            </div>
+          </div>
+
+          <div className="gridHeadSingle">
+            <a>Url</a>
+            <div className="gridHeadIcons">
+              <AiFillCaretUp className="IconUp" />
+              <AiFillCaretDown className="IconDown" />
+            </div>
+          </div>
+
+        </div>
+
+        <div className="headLine"></div>
+        <div>
+          {dummyData
+            .slice(
+              paginationPage * selectPage - selectPage,
+              paginationPage * selectPage
+            )
+            .filter((item) =>
+              item.title.toLowerCase().includes(search.toLowerCase())
+            )
+            .map((row) => {
+              return (
+                <div>
+                  <div className="flex-even gridBody">
+                    <div className="firstColumn" style={{ gap: "20px" }}>
+                      <a>{row?.title}</a>
+                    </div>
+
+                    <a>{row?.location}</a>
+                    <a>{row.description.slice(0,100)}...</a>
+                    <a className="clientDate" onClick={()=>openInNewTab(row?.url)}>{row.url}</a>
+                  </div>
+                  <div className="RowsLine"></div>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GridMain;
---
+++ src/features/pageSlice.jsx
@@ -0,0 +1,86 @@
+// This is the Store to add the common data for the app and the store Entry Point is in the store/Store.jsx and the whole store in wrapped in the parent of the whole app which is main.jsx
+
+import { createSlice } from "@reduxjs/toolkit";
+
+let initialState = {
+  paginationPage: 1,
+  selectPage: 10,
+  inputPage: 1,
+  search: "",
+  totalPages: 10,
+  data: [],
+  titleSort: false,
+  descriptionSort: false,
+  locationSort: false,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    handlePaginationPage: (state, action) => {
+      state.paginationPage = action.payload.page;
+    },
+    handleSelectPage: (state, action) => {
+      state.selectPage = action.payload.page;
+      state.paginationPage = 1;
+    },
+    handleInputPage: (state, action) => {
+      state.inputPage = action.payload.page;
+    },
+    handleSearch: (state, action) => {
+      state.search = action.payload.search;
+    },
+    handleTotalPages: (state, action) => {
+      state.totalPages = action.payload.pages;
+    },
+    setData: (state, action) => {
+      state.data = action.payload.data;
+      state.totalPages=Math.ceil( action.payload.data.length / state.selectPage)
+    },
+    handleInputForm: (state) => {
+      if (Number(state.inputPage) <= state.totalPages) {
+        state.paginationPage = Number(state.inputPage);
+      }
+    },
+    handleTitleSort: (state) => {
+      if (state.titleSort) {
+        state.data = state.data.sort((a, b) => a.title > b.title);
+      } else {
+        state.data = state.data.sort((a, b) => a.title < b.title);
+      }
+      state.titleSort = !state.titleSort;
+    },
+    handleDescriptionSort: (state) => {
+      if (state.descriptionSort) {
+        state.data = state.data.sort((a, b) => a.description > b.description);
+      } else {
+        state.data = state.data.sort((a, b) => a.description < b.description);
+      }
+      state.descriptionSort = !state.descriptionSort;
+    },
+    handleLocationSort: (state) => {
+      if (state.locationSort) {
+        state.data = state.data.sort((a, b) => a.location > b.location);
+      } else {
+        state.data = state.data.sort((a, b) => a.location < b.location);
+      }
+      state.locationSort = !state.locationSort;
+    },
+  },
+});
+
+export const {
+  handlePaginationPage,
+  handleInputPage,
+  handleSelectPage,
+  handleSearch,
+  handleTotalPages,
+  setData,
+  handleInputForm,
+  handleDescriptionSort,
+  handleLocationSort,
+  handleTitleSort
+} = userSlice.actions;
+
+export default userSlice.reducer;
